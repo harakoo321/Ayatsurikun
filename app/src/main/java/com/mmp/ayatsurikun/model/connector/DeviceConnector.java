541de73854fcc0ab5b0c678149ca40a375eca852
harakoo321 <kouseih321@gmail.com>
2023-11-27 01:47:05 +0900

    fix:SignalButtonsActivityが再描画されるたびに新しいSignalButtonsViewModelのインスタンスが生成されてしまう問題、ボタン追加ダイアログを表示中に画面回転するとクラッシュしてしまう問題の修正

--- app/src/main/java/com/mmp/ayatsurikun/model/connector/DeviceConnector.java
+++ app/src/main/java/com/mmp/ayatsurikun/model/connector/DeviceConnector.java
@@ -2,8 +2,10 @@ package com.mmp.ayatsurikun.model.connector;
 
 import androidx.lifecycle.LiveData;
 
+import com.mmp.ayatsurikun.contract.SignalButtonsContract;
+
 public interface DeviceConnector {
-    void setUp();
+    void setUp(SignalButtonsContract contract);
     void connect();
     void disconnect();
     void send(byte[] signal);
